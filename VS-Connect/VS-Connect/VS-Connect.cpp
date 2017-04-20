54fc6f6cd5b8ed26ca1f25053c910be6ce21a8b3
old-node <olivier.ld@gmail.com>
2017-04-20 11:52:05 -0400

    Test officiel d'accès GitHub avec Visual Studio

---
+++ VS-Connect/VS-Connect/VS-Connect.cpp
@@ -0,0 +1,30 @@
+//Test officiel d'accès GitHub avec Visual Studio
+
+#include <iostream>
+using namespace std;
+
+
+/* Fonctions.
+==========*/
+//Vide le buffer avant chaque saisie.
+void viderBuffer() {
+	cin.clear();
+	cin.seekg(0, ios::end);
+
+	//Si le flux est dans un état invalide, on le remet valide
+	if (!cin.fail())	//Flux valide, donc le buffer est non vide
+		cin.ignore(numeric_limits<streamsize>::max());
+	else				//Flux invalide, donc le buffer est vide
+		cin.clear();
+}
+
+//Attend que l'utilisateur pèse une touche.
+void pause() {
+	viderBuffer();
+	cin.ignore();
+}
+
+int main() {
+	cout << "Ligne 1";
+	pause();
+}
